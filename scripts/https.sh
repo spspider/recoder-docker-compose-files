3ff5ecad96c3c7f6145ce7d5d8d29549982a607f
recoder <recoder>
2025-06-08 22:51:40 +0300

    Update Docker and Nginx configurations for new wordpress

--- scripts/https.sh
+++ scripts/https.sh
@@ -1,2 +1,2 @@
 sudo apt install certbot python3-certbot-nginx -y
-sudo certbot --nginx -d recmy.pp.ua -d see.recmy.pp.ua -d node.recmy.pp.ua -d tor.recmy.pp.ua -d graf.recmy.pp.ua -d home.recmy.pp.ua
+sudo certbot certonly --standalone -d recmy.pp.ua -d see.recmy.pp.ua -d node.recmy.pp.ua -d tor.recmy.pp.ua -d graf.recmy.pp.ua -d home.recmy.pp.ua -d gica-paint.pp.ua -d ecologic.pp.ua
