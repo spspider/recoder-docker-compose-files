3fc74930abbe7f98c37bbe7e426985edbb7c289f
recoder <recoder>
2025-06-08 11:10:13 +0300

    Update configuration files and scripts for improved functionality and organization

---
+++ scripts/https.sh
@@ -0,0 +1,2 @@
+sudo apt install certbot python3-certbot-nginx -y
+sudo certbot --nginx -d recmy.pp.ua -d see.recmy.pp.ua -d node.recmy.pp.ua -d tor.recmy.pp.ua -d graf.recmy.pp.ua -d home.recmy.pp.ua
